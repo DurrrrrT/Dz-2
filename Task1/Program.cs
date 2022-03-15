be810fca76d8419635d84f7796d232c3f39b9966
Amir <amir200320032003@gmail.com>
2022-03-15 21:21:21 +0300

    Dz

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿string number = Console.ReadLine();
+while (number.Length != 3)
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число ");
+    number = Console.ReadLine();
+}
+System.Console.WriteLine(number[1]);
+
---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// System.Console.WriteLine("Введите число ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// int numberB = Convert.ToInt32(0);
+// int numberC = Convert.ToInt32(7);
+// int numberD = Convert.ToInt32(23);
+
+// if (numberA % 7 ==0 & numberA % 23 == 0)
+// System.Console.WriteLine("Кратно 7 и 23 ");
+// else
+// {
+// System.Console.WriteLine("Не кратно ");
+// }
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 ==0 & number % 23 == 0)
+    {
+        System.Console.WriteLine("кратное 7 и 23");
+    }
+    else 
+    {
+        System.Console.WriteLine("не кратное ");
+    }
+
