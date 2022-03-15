89b78f724d7ac2058599a8b9ff2bb5c0f4f36fdd
Amir <amir200320032003@gmail.com>
2022-03-15 22:02:23 +0300

    Добавлен новый вариант решения 1 зr2\OneDrive\адачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,8 +1,15 @@
-﻿string number = Console.ReadLine();
-while (number.Length != 3)
-{
-    System.Console.WriteLine("Вы ввели не трехзначное число ");
-    number = Console.ReadLine();
-}
-System.Console.WriteLine(number[1]);
+﻿// string number = Console.ReadLine();
+// while (number.Length != 3)
+// {
+//     System.Console.WriteLine("Вы ввели не трехзначное число ");
+//     number = Console.ReadLine();
+// }
+// System.Console.WriteLine(number[1]);
 
+int number = Convert.ToInt32(Console.ReadLine());
+while (number >=1000 || number <= 99)
+{
+    System.Console.WriteLine("Введите трехзначное число ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine(number / 10 % 10);
